e057a06511e8c7c2175636430f9017beb67bfb00
Chris Eicher <chrisaeicher@gmail.com>
2023-04-16 20:26:44 -0700

    replaced standard Next.js stuff. Added new packages. Added icon component.

---
+++ src/components/atoms/Icon/Icon.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Icon.module.scss';
+
+type sizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+interface IconProps {
+	name: string;
+	size?: sizes;
+	className?: string;
+	props?: any;
+}
+
+export default function Icon(IconProps: IconProps) {
+	const iconProps = { ...IconProps };
+	const size = iconProps.size || 'md';
+	console.log(styles);
+	const classNames =
+		`${styles.Icon} ${styles[size]}` +
+		(iconProps?.className ? ` ${iconProps.className}` : '');
+
+	const IconSrc = require(`@/icons/${iconProps.name}`).default;
+
+	return (
+		<div className={classNames} {...iconProps.props}>
+			<IconSrc />
+		</div>
+	);
+}
---
+++ src/components/index.tsx
@@ -0,0 +1,1 @@
+export { default as Icon } from './atoms/Icon/Icon';
